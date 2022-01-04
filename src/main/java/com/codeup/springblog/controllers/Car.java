e144171e23e4ea5867ff98a9cf9553f94a4fbfc0
Bernie Esquivel <bernie.esquivel01@gmail.com>
2022-01-04 11:27:00 -0600

    Repositories and JPA lecture

---
+++ src/main/java/com/codeup/springblog/controllers/Car.java
@@ -0,0 +1,44 @@
+package com.codeup.springblog.controllers;
+
+import javax.persistence.*; //brings in 'all' imports
+
+
+@Entity
+@Table(name="cars") //creates another table with specified name
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 50)
+    private String make;
+
+    @Column (nullable = false, length = 50)
+    private String model;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+}
